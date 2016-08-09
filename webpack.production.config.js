f5ff1155208f08b90e4b153f194e2cc4826f0e55
Andrew Hoskins <andrew.hoskins16@gmail.com>
2016-08-09 16:19:46 -0700

    initial commit

---
+++ webpack.production.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack')
+var path = require('path')
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  devtool: 'cheap-source-map',
+  entry: [
+    path.resolve(__dirname, 'app/main.jsx')
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    publicPath: '/',
+    filename: './bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, include: path.resolve(__dirname, 'app'), loader: 'style-loader!css-loader' },
+      { test: /\.js[x]?$/, include: path.resolve(__dirname, 'app'), exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new CopyWebpackPlugin([
+      { from: './app/index.html', to: 'index.html' },
+      { from: './app/main.css', to: 'main.css' }
+    ])
+  ]
+}
